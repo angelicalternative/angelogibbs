f3407dc8c6da0db19ba3e05f1829f3a35a18403b
angelicalternative <angelicalternative@gmail.com>
2026-09-05 16:08:16 -0500

    Delete portfolio directory

--- portfolio/serve.py
+++
@@ -1,9 +0,0 @@
-import http.server
-import os
-
-PORT = 4890
-os.chdir(os.path.dirname(os.path.abspath(__file__)))
-http.server.test(
-    HandlerClass=http.server.SimpleHTTPRequestHandler,
-    port=PORT,
-)
